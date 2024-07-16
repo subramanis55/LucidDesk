20a338f7b89b757051d5cc83c7d9275d7f06d97b
subramanis55 <subramanis6385@gmail.com>
2024-07-16 18:34:47 +0530

    Intial

---
+++ LucidDesk/Manager/Classes/Desk.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace LucidDesk.Manager
+{
+    public class Desk : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private int id;
+        private string iPAddress;
+        private string hostName;
+        private string profileName;
+        private BitmapImage profileImage;
+        private BitmapImage desktopImage;
+        private string password;
+        private string macAddress;
+        private string osName;
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+                OnPropertyChanged(nameof(Id));
+            }
+        }
+        public string IPAddress{
+        get{
+                return iPAddress;
+        }
+        set{
+                iPAddress = value;
+                OnPropertyChanged(nameof(IPAddress));
+        }
+        }
+        public string HostName
+        {
+            get
+            {
+                return hostName;
+            }
+            set
+            {
+                hostName = value;
+                OnPropertyChanged(nameof(HostName));
+            }
+        }
+        public string DeskUserName
+        {
+            get
+            {
+                return profileName;
+            }
+            set
+            {
+                profileName = value;
+                OnPropertyChanged(nameof(DeskUserName));
+            }
+        }
+        public BitmapImage ProfileImage
+        {
+            get
+            {
+                return profileImage;
+            }
+            set
+            {
+                profileImage = value;
+                OnPropertyChanged(nameof(ProfileImage));
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+        public string MacAddress
+        {
+            get
+            {
+                return macAddress;
+            }
+            set
+            {
+                macAddress = value;
+                OnPropertyChanged(nameof(MacAddress));
+            }
+        }
+        public BitmapImage DesktopImage
+        {
+            get
+            {
+                return desktopImage;
+            }
+            set
+            {
+                desktopImage = value;
+                OnPropertyChanged(nameof(DesktopImage));
+            }
+        }
+        public string OsName{
+            get
+            {
+                return osName;
+            }
+            set
+            {
+                osName = value;
+                OnPropertyChanged(nameof(OsName));
+            }
+        }
+        protected virtual void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ LucidDesk/UserControls/ProfileU.xaml.cs
@@ -0,0 +1,94 @@
+﻿using LucidDesk.Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LucidDesk.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ProfileU.xaml
+    /// </summary>
+    public partial class ProfileU : UserControl
+    {
+        private Desk desk;
+
+        public string DeskUserName
+        {
+            get { return (string)GetValue(DeskUserNameProperty); }
+            set { SetValue(DeskUserNameProperty, value); }
+        }
+
+        public static readonly DependencyProperty DeskUserNameProperty =
+            DependencyProperty.Register("DeskUserName", typeof(string), typeof(ProfileU), new PropertyMetadata("Name"));
+
+
+        public string PCName
+        {
+            get { return (string)GetValue(PCNameProperty); }
+            set { SetValue(PCNameProperty, value); }
+        }
+
+        public static readonly DependencyProperty PCNameProperty =
+            DependencyProperty.Register("PCName", typeof(string), typeof(ProfileU), new PropertyMetadata("PcName"));
+
+
+
+        public string DeskOSName
+        {
+            get { return (string)GetValue(DeskOSNameProperty); }
+            set { SetValue(DeskOSNameProperty, value); }
+        }
+
+        public static readonly DependencyProperty DeskOSNameProperty =
+            DependencyProperty.Register("DeskOSName", typeof(string), typeof(ProfileU), new PropertyMetadata("Windows"));
+
+
+
+        public string DeskId
+        {
+            get { return (string)GetValue(DeskIdProperty); }
+            set { SetValue(DeskIdProperty, value); }
+        }
+
+        public static readonly DependencyProperty DeskIdProperty =
+            DependencyProperty.Register("DeskId", typeof(string), typeof(ProfileU), new PropertyMetadata(""));
+
+        public Desk Desk
+        {
+            set
+            {
+                desk = value;
+                DeskUserName = desk.DeskUserName;
+                PCName = Desk.HostName;
+                DeskOSName = Desk.OsName;
+                DeskId =""+Desk.Id;
+                DeskProfileImageComponent.Image = Desk.ProfileImage;
+                DesktopImageComponent.Source = Desk.DesktopImage;
+            }
+            get
+            {
+                return desk;
+            }
+        }
+
+
+
+
+
+        public ProfileU()
+        {
+            InitializeComponent();
+        }
+    }
+}
