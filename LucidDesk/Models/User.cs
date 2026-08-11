e679c214d6e3318f8d1c86b7962c879749d0e480
subramanis55 <subramanis6385@gmail.com>
2026-08-11 08:50:40 +0530

    API Service class added

---
+++ LucidDesk/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucidDesk.Models
+{
+    class User
+    {
+        public Guid UserID { get; set; }
+        public string UserNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ LucidDesk/Services/UserService.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace LucidDesk.Services
+{
+    public static class UserService
+    {
+        public static async bool CreateUserAsync(User user)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.PostAsJsonAsync("https://localhost:5001/api/users", user);
+                return response.IsSuccessStatusCode;
+            }
+        }
+    }
+}
+
+
